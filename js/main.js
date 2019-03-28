e8e76023e73430a56bd108a2e4ccfe4d15e4b0e0
mtmackenzie-01 <49035445+mtmackenzie-01@users.noreply.github.com>
2019-03-28 15:35:25 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var btnSection = document.getElementById('buttonsection');
+
+btnSection.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  alert("Order submitted. Thank you!")
+})
